ce1a980cedf18f4896784823b49530dbd0cea227
vanessa corredor <Vanessa.estefania.corredor>
2020-01-03 11:00:39 -0500

    se añadio un parrafo en la pagina de articulos en cual solo muestra del primer parrafo de cada articulo 150 carateres

--- src/paginas/Articulos.js
+++ src/paginas/Articulos.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Articulo from './Articulo';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ArticleContent from './article-content';
-const Articulos = ()=>(
+const Articulos = () => (
     <>
-<h1>Articulos </h1>  
-{ArticleContent.map(
-    (article,key )=>(
-    <Link className="article-list-item" key ={key} to={`/articulo/${article.name}`}><h3>{article.title}</h3></Link>
-    )
-)}
+        <h1>Articulos </h1>
+        {ArticleContent.map(
+            (article, key) => (
+                <Link className="article-list-item" key={key} to={`/articulo/${article.name}`}><h3>{article.title}
+                </h3>
+                    <p>{article.content[0].substring(0, 150)}</p>
+                </Link>
+
+            )
+        )}
     </>
 );
 export default Articulos;
