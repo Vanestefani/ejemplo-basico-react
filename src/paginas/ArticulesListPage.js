db43285b8ea7615cb21e222ac1ee7bf0b907f02d
vanessa corredor <Vanessa.estefania.corredor>
2020-01-07 14:32:09 -0500

    añadiendo seccion otros articulos en la pagina articulo reusando el componete listaarticulos

--- src/paginas/ArticulesListPage.js
+++ src/paginas/ArticulesListPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ArticleContent from './article-content';
-import ListaArticulos from  '../Componentes/listaArticulos';
+import ListaArticulos from  '../Componentes/ListaArticulos';
 const ArticulesListPage = () => (
  <ListaArticulos articulos={ArticleContent}/>
 );
--- src/paginas/Articulo.js
+++ src/paginas/Articulo.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ArticleContent from './article-content';
+import ListaArticulos from '../Componentes/ListaArticulos';
 const Articulo = ({match}) =>{
     //url guarda el parametro name  en una constante name
     const name =match.params.name;
-   
+      //arreglo que guarda la lista de articulos diferentes al
+      const otrosarticulos= ArticleContent.filter ((articulos , key)=>(articulos.name !=name));
     const article=ArticleContent.find(article=>(article.name===name));
     if(!article) return(<h1>Este articulo no existe  </h1>)
     return(
@@ -14,6 +16,8 @@ const Articulo = ({match}) =>{
 <p key={key}>{paragraph}</p>
 
     ))}
+    <h3>Otros Articulos</h3>
+    <ListaArticulos articulos={otrosarticulos}/>
 </>
 
 )};
